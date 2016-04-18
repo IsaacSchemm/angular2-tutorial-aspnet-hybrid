709d7dba42425eab5e79da497abe72d43d83e4b3
Isaac Schemm <isaacschemm@gmail.com>
2016-04-18 13:25:16 -0500

    Add ASP.NET MVC 4 sample project

---
+++ AspNetHybrid/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetHybrid.MvcApplication" Language="C#" %>
---
+++ AspNetHybrid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetHybrid.Startup))]
+namespace AspNetHybrid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
