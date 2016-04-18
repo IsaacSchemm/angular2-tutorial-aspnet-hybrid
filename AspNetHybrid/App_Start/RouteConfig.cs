7f5e07fdd8a1d9cb837b898a13b83f3f07145174
Isaac Schemm <isaacschemm@gmail.com>
2016-04-18 15:03:14 -0500

    Auto-copy stuff from ASP.NET Core project into ASP.NET 4 project

--- AspNetHybrid/App_Start/RouteConfig.cs
+++ AspNetHybrid/App_Start/RouteConfig.cs
@@ -10,6 +10,13 @@ namespace AspNetHybrid {
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // The Angular2 application here is set up in such a way so that it expects relative paths to be relative to the root.
+            routes.MapRoute(
+                name: "HeroesDemo",
+                url: "Heroes",
+                defaults: new { controller = "Heroes", action = "Index" }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ AspNetHybrid/Controllers/HeroesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetHybrid.Controllers
+{
+    public class HeroesController : Controller
+    {
+        // GET: Heroes
+        public ActionResult Index()
+        {
+            if (Request.Url.AbsolutePath != "/Heroes") {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
